051fc5839ed051a0a55b827e3de42783cec60921
Tristan Davey <tristan@eri-162-144.d.port.ac.uk>
2015-02-07 16:51:30 +0000

    Full working without ui

--- js/main.js
+++ js/main.js
@@ -1,47 +1,50 @@
 var startLat;
 var startLon;
-var map = L.map('map').setView([startLat, startLon], 13);
+var map;
 var routing;
-
-L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    id: 'examples.map-i875mjb7'
-}).addTo(map);
-
-L.marker([startLat, startLon]).addTo(map);
 
 function CalculateRoute(postcode, holeNum) {
 
-getPubsFromUserLocation(postcode, 30, function (e) {});
+    getPubsFromUserLocation(postcode, 30, function (e) {
 
-startLat = e.location.Latitude;
-startLon = e.location.Longitude;
+        startLat = e.location.Latitude;
+        startLon = e.location.Longitude;
 
-var name;
-var waypoints = [];
+        map = L.map('map').setView([startLat, startLon], 13);
 
-for (var i = 0; i < e.length - 1; i++) {
+        L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            id: 'examples.map-i875mjb7'
+        }).addTo(map);
 
-    name = e[i].display_name.split(",")[0];
+        var name;
+        var waypoints = [];
 
-    waypoints[i] = L.latLng(e[i].lat, e[i].lon);
-    waypoints[i].name = name;
+        for (var i = 0; i < e.pubs.length - 1; i++) {
 
-}
-routing = L.Routing.control({
-    plan: L.Routing.plan(getClosestPubs(waypoints, holeNum), {
-        createMarker: function (i, wp) {
-            return calculateMarkers(i, wp);
+            name = e.pubs[i].Name.split(",")[0];
+
+            waypoints[i] = L.latLng(e.pubs[i].Lat, e.pubs[i].Long);
+            waypoints[i].name = name;
+
         }
-    }),
-    draggableWaypoints: false,
-    addWaypoints: false
-}).addTo(map);
+        routing = L.Routing.control({
+            plan: L.Routing.plan(getClosestPubs(waypoints, holeNum), {
+                createMarker: function (i, wp) {
+                    return calculateMarkers(i, wp);
+                }
+            }),
+            draggableWaypoints: false,
+            addWaypoints: false
+        }).addTo(map);
 
-});
+
+    });
+
+};
 
 function calculateMarkers(i, wp) {
 
@@ -99,3 +102,5 @@ function SortByDist(a, b) {
     var bDist = b.distance;
     return ((aDist < bDist) ? -1 : ((aDist > bDist) ? 1 : 0));
 }
+
+CalculateRoute("me157ed", 9);
